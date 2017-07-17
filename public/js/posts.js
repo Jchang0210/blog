b71a68c97a27bb1fc829caa947914d683e5ccace
yh0210 <johnnychang0210@gmail.com>
2017-07-17 15:44:49 +0800

    performance enhancement

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,5 +1,5 @@
 window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+    $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
         $(this).remove();
     });
 }, 4000);
